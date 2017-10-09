e8539692b23401b1aafb3616125f2a9529ae8493
Nick Randall <nick.randall@domo.com>
2017-10-09 15:56:30 -0600

    docs: adding updated docs to readme

--- src/index.js
+++ src/index.js
@@ -81,6 +81,20 @@ function dispatchQueue(client: QueryBatcher, options: Options): void {
  * @param {integer} options.batchInterval   - duration (in MS) of each batch window. (default 6)
  * @param {integer} options.maxBatchSize    - max number of requests in a batch. (default 0)
  * @param {boolean} options.defaultHeaders  - default headers to include with every request
+ *
+ * @example
+ * const fetcher = batchedQuery => return fetch('path/to/graphql', {
+ *    method: 'post',
+ *    headers: {
+ *      Accept: 'application/json',
+ *      'Content-Type': 'application/json',
+ *    },
+ *    body: JSON.stringify(batchedQuery),
+ *    credentials: 'include',
+ * })
+ * .then(response => response.json())
+ *
+ * const client = new QueryBatcher(fetcher, { maxBatchSize: 10 })
  */
 export default class QueryBatcher {
   fetcher: Fetcher
@@ -107,6 +121,20 @@ export default class QueryBatcher {
    * @param {Options}     overrides      - the client options overrides.
    *
    * @return {promise} resolves to parsed json of server response
+   *
+   * @example
+   * client.fetch(`
+   *    query getHuman($id: ID!) {
+   *      human(id: $id) {
+   *        name
+   *        height
+   *      }
+   *    }
+   * `, { id: "1001" }, 'getHuman')
+   *    .then(human => {
+   *      // do something with human
+   *      console.log(human);
+   *    });
    */
   fetch(
     query: string,
@@ -148,6 +176,20 @@ export default class QueryBatcher {
    * @param {Options}     overrides      - the client options overrides.
    *
    * @return {Promise<Array<Result>>} resolves to parsed json of server response
+   *
+   * @example
+   * client.forceFetch(`
+   *    query getHuman($id: ID!) {
+   *      human(id: $id) {
+   *        name
+   *        height
+   *      }
+   *    }
+   * `, { id: "1001" }, 'getHuman')
+   *    .then(human => {
+   *      // do something with human
+   *      console.log(human);
+   *    });
    */
   forceFetch(
     query: string,
